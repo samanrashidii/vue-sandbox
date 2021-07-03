51bbd3be86fe7d1fbf76324a0b5e5847b96de097
Saman Rashidi <samanrashidi44@gmail.com>
2021-07-03 21:26:28 +0430

    feature: created our first component

---
+++ src/components/Logo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="logo">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>
+      {{ fullName }}
+    </p>
+    <p>
+      {{ 'Age : ' + age }}
+    </p>
+    <h1>
+      {{ fullName }}
+    </h1>
+    <button @click="callAlert(fullName)">
+      Click me
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  data () {
+    return {
+      firstname: 'Saman',
+      lastname: 'Rashidi',
+      age: 31
+    }
+  },
+  computed: {
+    fullName () {
+      const output = this.firstname + ' ' + this.lastname
+      return output
+    }
+  },
+  methods: {
+    callAlert (message) {
+      alert(message)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Logo />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Logo from '@/components/Logo.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Logo
   }
 }
 </script>
