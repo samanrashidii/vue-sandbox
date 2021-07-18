dda4799821dc773baa77944606b50572918eee94
Saman Rashidi <samanrashidi44@gmail.com>
2021-07-18 21:15:46 +0430

    fetaure: added props and events to the component

---
+++ src/components/Sample.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="sample">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>
+      {{ 'Fullname : ' + fullName }}
+    </p>
+    <p>
+      {{ 'Age : ' + age }}
+    </p>
+    <p v-if="age > 30">
+      {{ firstname + ' is older than 30' }}
+    </p>
+    <p v-else>
+      {{ firstname + ' is younger than 30' }}
+    </p>
+    <p>
+      <strong>My favorite fruits : </strong>
+      <ul>
+        <li
+          v-for="(fruit, index) in fruits"
+          :key="index"
+        >
+          {{ fruit }}
+        </li>
+      </ul>
+    </p>
+    <button @click="changeAge(age)">
+      Click me to increase age
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sample',
+  props: {
+    firstname: {
+      type: String,
+      default: ''
+    },
+    lastname: {
+      type: String,
+      default: ''
+    },
+    age: {
+      type: Number,
+      default: 0
+    },
+    fruits: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    fullName () {
+      const output = this.firstname + ' ' + this.lastname
+      return output
+    }
+  },
+  methods: {
+    changeAge (age) {
+      this.$emit('age', age)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="home">
-    <Logo />
+    <Sample
+      :firstname="firstname"
+      :lastname="lastname"
+      :age="age"
+      :fruits="fruits"
+      @age="changeAge"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Logo from '@/components/Logo.vue'
+import Sample from '@/components/Sample.vue'
 
 export default {
   name: 'Home',
   components: {
-    Logo
+    Sample
+  },
+  data () {
+    return {
+      firstname: 'Alireza',
+      lastname: 'Morgan',
+      age: 24,
+      fruits: ['apple', 'orange', 'strawberry', 'peach']
+    }
+  },
+  methods: {
+    changeAge (age) {
+      this.age = age + 1
+    }
   }
 }
 </script>
